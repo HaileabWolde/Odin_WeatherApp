a287d1cb2734334771a6ee1fd7be98523d9d0374
Haileab <womahaileab1428@gmail.com>
2025-07-16 22:02:36 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import "./styles.css";
-import HomePage from "./Home";
-import MenuPage from "./Menu";
-import contactPage from "./Contact";
-const navbarButton = document.querySelector(".navbar");
-navbarButton.addEventListener("click", (event) => {
-    const target = event.target;
-    if (target.id === "home") {
-        HomePage.render();
-    } else if (target.id === "menu") {
-        MenuPage.render();
-    } else if (target.id === "contact") {
-        contactPage.render();
-    }
-});
-
-console.log("Starting Odin Restaurant...");
+const greeting = "Hello, Odinite!";
+console.log(greeting);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -9,22 +10,22 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-   devtool: "eval-source-map",
+  devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
   },
-   plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
-   module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-     {
+      {
         test: /\.html$/i,
         loader: "html-loader",
       },
