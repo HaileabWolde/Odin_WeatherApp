17595462b2f4ac7d4042f708a6a8f1f4187641fe
Haileab <womahaileab1428@gmail.com>
2025-07-17 11:15:19 +0300

    started working with apis for the weather app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+const APIKEY = 'WCVGTG5MW2HXEPAUAUJJWGHPA';
+const fa_search = document.querySelector('.fa-search');
+const cityInput = document.getElementById('city');
+
+
+async function fetchWeatherData(city) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${APIKEY}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+fa_search.addEventListener('click', () => {
+    const city = cityInput.value;
+    if (city) {
+        fetchWeatherData(city);
+    } else {
+        alert('Please enter a city name');
+    }});
